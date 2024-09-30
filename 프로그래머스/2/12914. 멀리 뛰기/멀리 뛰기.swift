b852f503615d66dde1e0d9497ed4aa4d827b5356
Yaewon Lee <yw990324@gmail.com>
2024-09-30 06:39:56 +0200

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.swift
@@ -0,0 +1,20 @@
+func solution(_ n:Int) -> Int {
+    if n == 1 {
+        return 1
+    } else if n == 2 {
+        return 2
+    }
+    
+    var dp = Array(repeating: 0, count: n + 1)
+    
+    dp[1] = 1
+    dp[2] = 2
+    
+    if n >= 3 {
+        for i in 3...n {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+        }   
+    }
+    
+    return dp[n]
+}
