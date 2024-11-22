21137d3335b250aa4153ad81fab80075f1af4260
Yaewon Lee <yw990324@gmail.com>
2024-11-22 22:16:05 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -1,30 +1,14 @@
-import Foundation
+var dp = Array(repeating: 0, count: 12)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
-let T = Int(readLine()!)!
+for i in 4...11 {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+}
 
-for _ in 0..<T {
+let t = Int(readLine()!)!
+for _ in 0..<t {
     let n = Int(readLine()!)!
-    
-    if n == 1 {
-        print(1)
-        continue
-    } else if n == 2 {
-        print(2)
-        continue
-    } else if n == 3 {
-        print(4)
-        continue
-    }
-    
-    var dp = Array(repeating: 0, count: n + 1)
-    
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 4
-    
-    for i in 4...n {
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
-    }
-    
     print(dp[n])
 }
